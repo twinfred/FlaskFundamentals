9a1d732b56ad5dcdc5645be612ad466c02de2c3e
Tim_Winfred <twinfred@gmail.com>
2018-04-05 12:49:02 -0700

    added a form and links to the index.html page

--- portfolio/portfolio.py
+++ portfolio/portfolio.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,5 +12,23 @@ def about():
 @app.route('/projects')
 def projects():
     return render_template('projects.html')
+
+@app.route('/submit')
+def submit():
+    return render_template('submit.html', name = "Tim")
+
+# FORM SUBMIT - POST METHOD
+# @app.route('/submit', methods=['POST'])
+# def show_user_profile():
+#     name = request.form['name']
+#     email = request.form['email']
+#     print name,email
+#     return redirect('/submit')
+
+# FORM SUBMIT - GET METHOD
+@app.route('/submit/<name>/<email>')
+def show_user_profile(name,email):
+    print name,email
+    return render_template('submit.html')
 
 app.run(debug=True)
