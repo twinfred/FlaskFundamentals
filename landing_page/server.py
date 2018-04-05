c4f0991024926209a984ea3c8520de2a18e605d0
Tim_Winfred <twinfred@gmail.com>
2018-04-05 14:03:04 -0700

    updated form and added a success page

--- landing_page/server.py
+++ landing_page/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -15,7 +16,13 @@ def dojos():
 @app.route('/submit', methods=["POST"])
 def submit_name():
     name = request.form['name']
+    location = request.form['location']
+    language = request.form['language']
+    comment = request.form['comment']
     print name
-    return redirect('/')
+    print location
+    print language
+    print comment
+    return render_template('success.html', name=name, location=location, language=language, comment=comment)
 
 app.run(debug=True)
