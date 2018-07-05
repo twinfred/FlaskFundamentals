66eef4daf4c691ee92377a490be6399088d88adc
Timothy Winfred <twinfred@gmail.com>
2018-07-05 00:23:51 -0700

    updated hello_world flask project to have query params, and get and post templating and data processing

--- hello_world/hello.py
+++ hello_world/hello.py
@@ -1,9 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from hey import fruit_qualities
+
 app = Flask(__name__)
 
-@app.route('/')
+users = []
+
+@app.route('/', methods=['GET'])
 
 def hello_world():
-    return render_template('index.html')
+    first = str(request.args.get("first"))
+    last = str(request.args.get("last"))
+    return render_template('index.html', users = users, first = first, last = last)
+
+@app.route('/', methods=['POST'])
+
+def add_params():
+    first = str(request.args.get("first"))
+    last = str(request.args.get("last"))
+    firstname = request.form.get('firstname')
+    print str(firstname)
+    lastname = request.form.get('lastname')
+    print str(lastname)
+    users.append({
+        "firstname": firstname,
+        "lastname": lastname,
+    })
+    return redirect("/?first="+first+"&last="+last)
 
 app.run(debug=True)
---
+++ hello_world/hey.py
@@ -0,0 +1,5 @@
+def fruit_qualities():
+    context = {
+        "mango": ["fruit", "juicy", "orange", "green", "red"]
+    }
+    return context
